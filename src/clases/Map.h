a43c04561f447bfd94d9dad5d83747f1b6adbb46
Peblo <a01275676@tec.mx>
2022-06-04 00:04:29 -0500

    test for compilation

--- src/clases/Map.h
+++ src/clases/Map.h
@@ -14,43 +14,44 @@
 
 struct Line
 {
-    sf::Vector2f pointA;
-    sf::Vector2f pointB;
+    sf::Vector2f pointA, pointB;
 };
 
 class Map
 {
 public:
     Map();
-    Map(const Player&,std::string);//,const Player&
+    Map(Player&,std::string,std::string);//,const Player&
     ~Map();
     sf::Sprite get();
     void update();
     void render(sf::RenderTarget&);
+    void checkCollision();
 private:
     sf::Sprite background;
-    sf::Sprite foreground;
+    //sf::Sprite foreground;
     sf::Texture texture;
     sf::Image wallMap;
     sf::Vector2f positon;
-    std::vector<Line> lines;
+    std::vector<Line> walls;
+    std::vector<Line> ceilings;
     Player* player;
     void initTexture();
     void initSprite();
     void initLines();
-    void checkCollision();
 };
 
 Map::Map()
 {
 }
 
-Map::Map(const Player& player,std::string path)
+Map::Map(Player& player_,std::string mapPath,std::string wallPath)
 {
-    this->wallMap.loadFromFile("assets/mapWalls/mapSpiral.png");
+    player = &player_;
+    this->wallMap.loadFromFile(wallPath);
     this->background.setPosition(sf::Vector2f(0.f,0.f));
     this->background.setScale(sf::Vector2f(4.f,4.f));
-    this->texture.loadFromFile(path);
+    this->texture.loadFromFile(mapPath);
     this->background.setTexture(this->texture);
     this->initLines();
 }
@@ -77,22 +78,80 @@ void Map::initSprite()
 }
 void Map::initLines()
 {
-    lines.push_back({sf::Vector2f(0,0),sf::Vector2f(0,700)});
-    // for(int i = 1; i < wallMap.getSize().x; i++)
-    // {
-    //     for(int j = 1; j < wallMap.getSize().y; j++)
-    //     {
-    //         if(wallMap.getPixel(j,i) == sf::Color::Black){}
-    //             //std::cout<<'#';
-    //         //else
-    //             //std::cout<<' ';
-    //     }
-    //     //std::cout<<'\n';
-    // }
+    /*
+    int blackPixels;
+    walls.push_back({sf::Vector2f(64,64),sf::Vector2f(64,640)});
+    walls.push_back({sf::Vector2f(640,64),sf::Vector2f(640,640)});
+    ceilings.push_back({sf::Vector2f(64,64),sf::Vector2f(640,64)});
+    ceilings.push_back({sf::Vector2f(64,640),sf::Vector2f(640,640)});
+    for(int i = 1; i < wallMap.getSize().x; i++)
+    {
+        for(int j = 1; j < wallMap.getSize().y; j++)
+        {
+            if(wallMap.getPixel(j,i) == sf::Color::Black)
+                std::cout<<'#';
+            else
+                std::cout<<' ';
+            
+            blackPixels = 0;
+            for(int k = 0; k<2;k++)
+            {
+                for(int l = 0; l<2;l++)
+                {
+                    if(wallMap.getPixel(j+k,i+l) == sf::Color::Black)
+                        blackPixels++;
+                }
+            }
+            if(blackPixels == 1)
+                std::cout<<blackPixels<<'\n';
+        }
+        std::cout<<'\n';
+    }
+    */
+    /*wallMap.getPixel(j,i) == sf::Color::Black &&
+            wallMap.getPixel(j+1,i) == sf::Color::Black &&
+            wallMap.getPixel(j,i+1) == sf::Color::Black &&
+            wallMap.getPixel(j+1,i+1) == sf::Color::White*/
 }
 void Map::checkCollision()
 {
-    
+    for(int i = 0; i<2; i++)
+    {
+        // For vertical walls
+        if(player->getPos().x<walls[i].pointA.x && 
+        player->getPos().x + player->getSize().x > walls[i].pointA.x &&
+        player->getPos().y < walls[i].pointB.y &&
+        player->getPos().y + player->getSize().y > walls[i].pointA.y)
+        {   
+            switch (player->getDirection())
+            {
+                case 'l':
+                    player->setPositionX(walls[i].pointA.x);
+                    break;
+                case 'r':
+                    player->setPositionX(walls[i].pointA.x-(player->getSize().x));
+                    break;
+            }
+        }   
+    }
+    for(int i = 0; i<2; i++)
+    {
+        if(player->getPos().y<ceilings[i].pointA.y &&
+        player->getPos().y+player->getSize().y>ceilings[i].pointA.y &&
+        player->getPos().x < ceilings[i].pointB.x &&
+        player->getPos().x + player->getSize().x>ceilings[i].pointA.x)
+        {
+            switch (player->getDirection())
+            {
+                case 'u':
+                    player->setPositionY(ceilings[i].pointA.y);
+                    break;
+                case 'd':
+                    player->setPositionY(ceilings[i].pointA.y-(player->getSize().y));
+                    break;
+            }
+        }
+    }
 }
 
 #endif
