713bb94382ecbdb63d985d32964b1de9610d6555
Peblo <a01275676@tec.mx>
2022-06-07 10:33:27 -0500

    Updated compilation for linux

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 g++ main.cpp -Isrc/clases -Isrc/include -o main -lsfml-graphics -lsfml-window -lsfml-system
+g++ main.cpp ./src/clases/Game.cpp ./src/clases/Enemy.cpp ./src/clases/Entity.cpp ./src/clases/Map.cpp ./src/clases/Player.cpp -Isrc/clases/ -Isrc/include -o main -lsfml-graphics -lsfml-window -lsfml-system
 ./main
